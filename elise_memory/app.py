b73a407764ad2505a39ed763ae7eef68adf1c9e1
brunofoxmulder <bruno@mbsimon.fr>
2026-09-18 22:13:03 +0200

    dev.3: bump API version

--- elise_memory/app.py
+++ elise_memory/app.py
@@ -11,7 +11,7 @@ from .store import MemoryStore
 DB_PATH = os.getenv("ELISE_MEMORY_DB", "/data/elise_memory.sqlite3")
 store = MemoryStore(DB_PATH)
 
-app = FastAPI(title="Élise Memory", version="0.1.0-dev.2")
+app = FastAPI(title="Élise Memory", version="0.1.0-dev.3")
 
 
 @app.on_event("startup")
